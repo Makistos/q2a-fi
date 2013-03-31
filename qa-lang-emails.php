4ecb4997d5fd9f8ee134dbdd67a3133eb53dae42
Marko Poutiainen <mep@sofistes.net>
2013-04-01 01:08:11 +0300

    Fixed encodings for some files.

--- qa-lang-emails.php
+++ qa-lang-emails.php
@@ -25,54 +25,54 @@
 */
 
 	return array(
-		'a_commented_body' => "Sinun kysymyksesi sivustolla ^site_title on saanut uuden kommentin käyttäjältä ^c_handle:\n\n^open^c_content^close\n\nVastauksesi oli:\n\n^open^c_context^close\n\nVoit vastata lisäämällä oman kommenttisi:\n\n^url\n\nKiitos,\n\n^site_title",
+		'a_commented_body' => "Sinun kysymyksesi sivustolla ^site_title on saanut uuden kommentin kÃ¤yttÃ¤jÃ¤ltÃ¤ ^c_handle:\n\n^open^c_content^close\n\nVastauksesi oli:\n\n^open^c_context^close\n\nVoit vastata lisÃ¤Ã¤mÃ¤llÃ¤ oman kommenttisi:\n\n^url\n\nKiitos,\n\n^site_title",
 		'a_commented_subject' => 'Sinun kysymyksesi sivustolla ^site_title on saanut uuden kommentin',
 
-		'a_followed_body' => "Sinun kysymykselläsi sivustolla ^site_title on uusi siihen liittyvä kysymys käyttäjältä ^q_handle:\n\n^open^q_title^close\n\nVastauksesi oli:\n\n^open^a_content^close\n\nKlikkaa alla olevaa linkkiä vastataksei uuteen kysymykseen:\n\n^url\n\nKiitos,\n\n^site_title",
-		'a_followed_subject' => 'Sinun kysymykselläsi sivustolla ^site_title on uusi siihen liittyvä kysymys',
+		'a_followed_body' => "Sinun kysymyksellÃ¤si sivustolla ^site_title on uusi siihen liittyvÃ¤ kysymys kÃ¤yttÃ¤jÃ¤ltÃ¤ ^q_handle:\n\n^open^q_title^close\n\nVastauksesi oli:\n\n^open^a_content^close\n\nKlikkaa alla olevaa linkkiÃ¤ vastataksei uuteen kysymykseen:\n\n^url\n\nKiitos,\n\n^site_title",
+		'a_followed_subject' => 'Sinun kysymyksellÃ¤si sivustolla ^site_title on uusi siihen liittyvÃ¤ kysymys',
 
-		'a_selected_body' => "Onnittelut! Vastauksesi sivustolla ^site_title on valittu parhaaksi käyttäjän ^s_handle toimesta:\n\n^open^a_content^close\n\nKysymys oli:\n\n^open^q_title^close\n\nKlikkaa alla olevaa linkkiä nähdäksesi vastauksesi:\n\n^url\n\nKiitos,\n\n^site_title",
+		'a_selected_body' => "Onnittelut! Vastauksesi sivustolla ^site_title on valittu parhaaksi kÃ¤yttÃ¤jÃ¤n ^s_handle toimesta:\n\n^open^a_content^close\n\nKysymys oli:\n\n^open^q_title^close\n\nKlikkaa alla olevaa linkkiÃ¤ nÃ¤hdÃ¤ksesi vastauksesi:\n\n^url\n\nKiitos,\n\n^site_title",
 		'a_selected_subject' => 'Vastauksesi sivustolla ^site_title on valittu!',
 
-		'c_commented_body' => "Uusi kommentti käyttäjältä ^c_handle on lisätty kommenttisi jälkeen sivustolla ^site_title:\n\n^open^c_content^close\n\nKeskustelu on seuraavanlainen:\n\n^open^c_context^close\n\nVita vastata lisäämällä uuden kommentin:\n\n^url\n\nKiitos,\n\n^site_title",
-		'c_commented_subject' => 'Kommenttisi sivustolla ^site_title on lisätty',
+		'c_commented_body' => "Uusi kommentti kÃ¤yttÃ¤jÃ¤ltÃ¤ ^c_handle on lisÃ¤tty kommenttisi jÃ¤lkeen sivustolla ^site_title:\n\n^open^c_content^close\n\nKeskustelu on seuraavanlainen:\n\n^open^c_context^close\n\nVita vastata lisÃ¤Ã¤mÃ¤llÃ¤ uuden kommentin:\n\n^url\n\nKiitos,\n\n^site_title",
+		'c_commented_subject' => 'Kommenttisi sivustolla ^site_title on lisÃ¤tty',
 
-		'confirm_body' => "Ole hyvä ja klikkaa alla olevaa linkkiä vahvistaaksesi sähköpostiosoitteesi sivustolle ^site_title.\n\n^url\n\nKiitos,\n^site_title",
-		'confirm_subject' => '^site_title - Sähköpostiosoiteen vahvistaminen',
+		'confirm_body' => "Ole hyvÃ¤ ja klikkaa alla olevaa linkkiÃ¤ vahvistaaksesi sÃ¤hkÃ¶postiosoitteesi sivustolle ^site_title.\n\n^url\n\nKiitos,\n^site_title",
+		'confirm_subject' => '^site_title - SÃ¤hkÃ¶postiosoiteen vahvistaminen',
 
-		'feedback_body' => "Kommentit:\n^message\n\nNimi:\n^name\n\nSähköposti:\n^email\n\nEdellinen sivu:\n^previous\n\nKäyttäjä:\n^url\n\nIP-osoite:\n^ip\n\nSelain:\n^browser",
+		'feedback_body' => "Kommentit:\n^message\n\nNimi:\n^name\n\nSÃ¤hkÃ¶posti:\n^email\n\nEdellinen sivu:\n^previous\n\nKÃ¤yttÃ¤jÃ¤:\n^url\n\nIP-osoite:\n^ip\n\nSelain:\n^browser",
 		'feedback_subject' => '^ palaute',
 
-		'flagged_body' => "Viesti käyttäjältä ^p_handle on vastaanotettu ^flags:\n\n^open^p_context^close\n\nKlikkaa alla olevaa linkkiä nähdäksesi viestin:\n\n^url\n\nKiitos,\n\n^site_title",
+		'flagged_body' => "Viesti kÃ¤yttÃ¤jÃ¤ltÃ¤ ^p_handle on vastaanotettu ^flags:\n\n^open^p_context^close\n\nKlikkaa alla olevaa linkkiÃ¤ nÃ¤hdÃ¤ksesi viestin:\n\n^url\n\nKiitos,\n\n^site_title",
 		'flagged_subject' => 'Sivustolla ^site_title on liputettu viesti',
 
-		'moderate_body' => "Viesti käyttäjältä ^p_handle vaatii hyväksyntääsi:\n\n^open^p_context^close\n\nKlikkaa alla olevaa linkkiä hyväksyäsi tai hylkääksesi viestin:\n\n^url\n\nKiitos,\n\n^site_title",
+		'moderate_body' => "Viesti kÃ¤yttÃ¤jÃ¤ltÃ¤ ^p_handle vaatii hyvÃ¤ksyntÃ¤Ã¤si:\n\n^open^p_context^close\n\nKlikkaa alla olevaa linkkiÃ¤ hyvÃ¤ksyÃ¤si tai hylkÃ¤Ã¤ksesi viestin:\n\n^url\n\nKiitos,\n\n^site_title",
 		'moderate_subject' => '^site_title moderointi',
 
-		'new_password_body' => "Uusi salasanasi sivustolle ^site_title on alla.\n\nSalasana: ^password\n\nSuosittelemme vaihtamaan tämän salasanan välittömästi kirjautumisen jälkeen.\n\nKiitos,\n^site_title\n^url",
+		'new_password_body' => "Uusi salasanasi sivustolle ^site_title on alla.\n\nSalasana: ^password\n\nSuosittelemme vaihtamaan tÃ¤mÃ¤n salasanan vÃ¤littÃ¶mÃ¤sti kirjautumisen jÃ¤lkeen.\n\nKiitos,\n^site_title\n^url",
 		'new_password_subject' => '^site_title - Uusi salasanasi',
 
-		'private_message_body' => "Käyttäjä ^f_handle on lähettänyt sinulle uuden viestin sivustolta  ^site_title:\n\n^open^message^close\n\n^moreKiitos,\n\n^site_title\n\n\nVoit torjua yksityisviestit tilisivultasi:\n^a_url",
-		'private_message_info' => "Lisätietoja käyttäjästä ^f_handle:\n\n^url\n\n",
-		'private_message_reply' => "Klikkaa alapuolella olevaa linkkiä vastataksesi käyttäjän ^f_handle yksityisviestiin:\n\n^url\n\n",
-		'private_message_subject' => 'Viesti käyttäjältä ^f_handle sivustolta ^site_title',
+		'private_message_body' => "KÃ¤yttÃ¤jÃ¤ ^f_handle on lÃ¤hettÃ¤nyt sinulle uuden viestin sivustolta  ^site_title:\n\n^open^message^close\n\n^moreKiitos,\n\n^site_title\n\n\nVoit torjua yksityisviestit tilisivultasi:\n^a_url",
+		'private_message_info' => "LisÃ¤tietoja kÃ¤yttÃ¤jÃ¤stÃ¤ ^f_handle:\n\n^url\n\n",
+		'private_message_reply' => "Klikkaa alapuolella olevaa linkkiÃ¤ vastataksesi kÃ¤yttÃ¤jÃ¤n ^f_handle yksityisviestiin:\n\n^url\n\n",
+		'private_message_subject' => 'Viesti kÃ¤yttÃ¤jÃ¤ltÃ¤ ^f_handle sivustolta ^site_title',
 
-		'q_answered_body' => "Käyttäjä ^a_handle on vastannut kysymykseesi sivustolla ^site_title:\n\n^open^a_content^close\n\nYour question was:\n\n^open^q_title^close\n\nJos pidät tästä vatauksesta, voit valita sen parhaaksi:\n\n^url\n\nKiitos,\n\n^site_title",
+		'q_answered_body' => "KÃ¤yttÃ¤jÃ¤ ^a_handle on vastannut kysymykseesi sivustolla ^site_title:\n\n^open^a_content^close\n\nYour question was:\n\n^open^q_title^close\n\nJos pidÃ¤t tÃ¤stÃ¤ vatauksesta, voit valita sen parhaaksi:\n\n^url\n\nKiitos,\n\n^site_title",
 		'q_answered_subject' => 'Kysymykseesi sivustolla ^site_title on tullut vastaus',
 
-		'q_commented_body' => "Käyttäjä ^c_handle on kommentoinut kysymystäsi sivustolla ^site_title:\n\n^open^c_content^close\n\nKysymyksesi oli:\n\n^open^c_context^close\n\nVoit vastata lisäämällä oman kommenttisi:\n\n^url\n\nKiitos,\n\n^site_title",
-		'q_commented_subject' => 'Kysymystäsi sivustolla^site_title on kommentoitu',
+		'q_commented_body' => "KÃ¤yttÃ¤jÃ¤ ^c_handle on kommentoinut kysymystÃ¤si sivustolla ^site_title:\n\n^open^c_content^close\n\nKysymyksesi oli:\n\n^open^c_context^close\n\nVoit vastata lisÃ¤Ã¤mÃ¤llÃ¤ oman kommenttisi:\n\n^url\n\nKiitos,\n\n^site_title",
+		'q_commented_subject' => 'KysymystÃ¤si sivustolla^site_title on kommentoitu',
 
-		'q_posted_body' => "Käyttäjä ^q_handle on kysynyt uuden kysymyksen:\n\n^open^q_title\n\n^q_content^close\n\nKlikkaa alla olevaa linkkiä nähdäksesi kysymyksen:\n\n^url\n\nKiitos,\n\n^site_title",
+		'q_posted_body' => "KÃ¤yttÃ¤jÃ¤ ^q_handle on kysynyt uuden kysymyksen:\n\n^open^q_title\n\n^q_content^close\n\nKlikkaa alla olevaa linkkiÃ¤ nÃ¤hdÃ¤ksesi kysymyksen:\n\n^url\n\nKiitos,\n\n^site_title",
 		'q_posted_subject' => 'Sivustolla ^site_title on uusi kysymys',
 
-		'reset_body' => "Ole hyvä ja klikkaa alla olevaa linkkiä asettaaksesi salasanasi uudestaan sivustolle ^site_title.\n\n^url\n\nVaihtoehtoisesti voit antaa alla olevan koodin silel tarkoitettuun kenttään.\n\nKoodi: ^code\n\nJos et pyytänyt salasanasi nollausta, voit jättää tämän viestin huomiotta.\n\nKiitos,\n^site_title",
+		'reset_body' => "Ole hyvÃ¤ ja klikkaa alla olevaa linkkiÃ¤ asettaaksesi salasanasi uudestaan sivustolle ^site_title.\n\n^url\n\nVaihtoehtoisesti voit antaa alla olevan koodin silel tarkoitettuun kenttÃ¤Ã¤n.\n\nKoodi: ^code\n\nJos et pyytÃ¤nyt salasanasi nollausta, voit jÃ¤ttÃ¤Ã¤ tÃ¤mÃ¤n viestin huomiotta.\n\nKiitos,\n^site_title",
 		'reset_subject' => '^site_title - Nollaa unohtunut salasana',
 
 		'to_handle_prefix' => "^,\n\n",
 
-		'welcome_body' => "Kiitos rekisteröitymisestäsi sivustolle ^site_title.\n\n^custom^confirmKirjautumistietosi ovat:\n\nSähköposti: ^email\nSalasana: ^password\n\nOle hyvä ja talleta nämä tiedot vastaisen varalle.\n\nKiitos,\n\n^site_title\n^url",
-		'welcome_confirm' => "Ole hyvä ja klikkaa alla olevaa linkkiä vahvistaaksesi sähköpostiosoitteesi.\n\n^url\n\n",
+		'welcome_body' => "Kiitos rekisterÃ¶itymisestÃ¤si sivustolle ^site_title.\n\n^custom^confirmKirjautumistietosi ovat:\n\nSÃ¤hkÃ¶posti: ^email\nSalasana: ^password\n\nOle hyvÃ¤ ja talleta nÃ¤mÃ¤ tiedot vastaisen varalle.\n\nKiitos,\n\n^site_title\n^url",
+		'welcome_confirm' => "Ole hyvÃ¤ ja klikkaa alla olevaa linkkiÃ¤ vahvistaaksesi sÃ¤hkÃ¶postiosoitteesi.\n\n^url\n\n",
 		'welcome_subject' => 'Tervetuloa sivustolle ^site_title!',
 	);
 	
